962e37e07e29c65ac48b0410b3825e81f3be75ea
azamatta9 <azamatta9@gmai.com>
2025-08-13 14:19:47 +0500

    ÐÑпервый коммит

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -13,8 +13,16 @@
 
 from functools import wraps
 
+
 def log(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        pass
+        arg = [str(a) for a in args]
+        kwarg = [f"{k}={v}" for k, v in kwargs.items()]
+        arg_kwarg = ", ".join(arg + kwarg)
+        print(f"Вызов: {func.__name__}({arg_kwarg})")
+        result = func(*args, **kwargs)
+        print(f"Результат: {result}")
+        return result
+
     return wrapper
